75748a540e2f99930e20fcc477c04616c8cc3a50
sayoriaaa <sayoriaaa@outlook.com>
2022-04-23 18:01:05 +0800

    add NFA simulation part

--- simple.c
+++ simple.c
@@ -5,6 +5,12 @@ Author:sayoriaaa
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#define MAX_LEN 256//use for stack
+
+int NFA_STATES=0;
+int NFA_START_STATE=0;
+int CHAR_SET_LEN=0;
+char char_set[MAX_LEN];
 
 int priority(char c){
     if(c=='*') return 3;
@@ -14,8 +20,8 @@ int priority(char c){
 }
 
 char *to_postfix(char *s){
-    char stack_o[256];
-    char *postfix = (char*)malloc(256*sizeof(char));
+    char stack_o[MAX_LEN];
+    char *postfix = (char*)malloc(MAX_LEN*sizeof(char));
     int stack_o_len=0, postfix_len=0;
     for(int i=0;i<strlen(s)-1;i++){
         if(s[i]!='('&&s[i]!='&'&&s[i]!='|'&&s[i+1]!=')'&&s[i+1]!='&'&&s[i+1]!='|'&&s[i+1]!='*'){
@@ -29,15 +35,13 @@ char *to_postfix(char *s){
             case ')':
                 while(stack_o[stack_o_len-1]!='(') postfix[postfix_len++]=stack_o[stack_o_len---1];
                 stack_o_len--;
-                break;
-            case '(':
-                stack_o[stack_o_len++]=s[i];
-                break;
+                break;          
             case '*':
             case '&':
             case '|':
                 while(stack_o_len>0&&priority(stack_o[stack_o_len-1])>=priority(s[i])) postfix[postfix_len++]=stack_o[stack_o_len---1];
-                stack_o[stack_o_len++]=s[i]; 
+            case '(':
+                stack_o[stack_o_len++]=s[i];
                 break;
             default:
                 postfix[postfix_len++]=s[i];                      
@@ -52,11 +56,11 @@ int getListLoc(char* s, char t){
     for(int i=0;i<strlen(s);i++){
         if(s[i]==t) return i;
     }
+    return -1;
 }
 
-void structDFA(char *s){//用一个邻接矩阵描述
+int** structDFA(char *s){//用一个邻接矩阵描述
     int state=0, char_set_len=0;
-    char char_set[256];
     for(int i=0;i<strlen(s);i++){
         if(s[i]!='|'&&s[i]!='&'&&s[i]!='*') char_set[char_set_len++]=s[i];
         for(int i=0;i<char_set_len-1;i++){
@@ -64,14 +68,15 @@ void structDFA(char *s){//用一个邻接矩阵描述
         }
     }
     char_set[char_set_len]='\0';
-    printf("\nchar set:%s\n",char_set); 
+    CHAR_SET_LEN=char_set_len;//save in global field 
 //***********获得输入字符集合****************//
-    int F[256][256];
+    int **F=(int**)malloc(sizeof(int*)*MAX_LEN);
+    for(int i=0;i<MAX_LEN;i++){
+        F[i]=(int*)malloc(sizeof(int)*(char_set_len+3));
+        memset(F[i], -1, sizeof(int)*(char_set_len+3));
+    }//initialize the transform function
 
-    for(int i=0;i<256;i++){
-        for(int j=0;j<256;j++) F[i][j]=-1;
-    }
-    int state_stack[256][3],state_stack_len=0;
+    int state_stack[MAX_LEN][3],state_stack_len=0;
     for(int i=0;i<strlen(s);i++){
         if(s[i]!='|'&&s[i]!='&'&&s[i]!='*'){
             int cor_pos=getListLoc(char_set,s[i]);
@@ -109,7 +114,7 @@ void structDFA(char *s){//用一个邻接矩阵描述
         }
     }
     
-    int state_visit[256]={0}, start_state=0;//to identify the start node
+    int state_visit[MAX_LEN]={0}, start_state=0;//to identify the start node
     for(int i=0;i<state;i++){
         printf("\nstate%d : ",i);
         for(int j=0;j<char_set_len+3;j++){
@@ -120,6 +125,8 @@ void structDFA(char *s){//用一个邻接矩阵描述
     for(int i=0;i<state;i++){
         if(state_visit[i]==0){start_state=i;break;}}
     printf("\n\nstart state:%d\n",start_state);
+    NFA_STATES=state;//save state to global field
+    NFA_START_STATE=start_state;
 
     FILE *f;
     f = fopen("nfa.dot","w+");
@@ -140,20 +147,70 @@ void structDFA(char *s){//用一个邻接矩阵描述
     fprintf(f,"}");
     fclose(f);
     system("dot -Tpng nfa.dot -o nfa.png");
+    return F;
+}
+
+void NFA_move(int **F, int state, int* oldStates, int* oldStates_len, int* alreadyOn){//handle epsilon for NFA_match
+    for(int i=0;i<3;i++){
+        int euqal_state=F[state][CHAR_SET_LEN+i];
+        if(euqal_state!=-1&&alreadyOn[euqal_state]==0){
+            alreadyOn[euqal_state]=1;
+            oldStates[*oldStates_len]=euqal_state;
+            (*oldStates_len)++;
+            NFA_move(F, euqal_state, oldStates, oldStates_len, alreadyOn);
+        }
+    }
+}
+
+int NFA_match(int** F, char *s){//test note for showing the legal state for each step of reading the input string
+    int oldStates[MAX_LEN]={0}, newStates[MAX_LEN]={0}, alreadyOn[MAX_LEN]={0};
+    int oldStates_len=1, newStates_len=0;
+    oldStates[0]=NFA_START_STATE;
+    alreadyOn[NFA_START_STATE]=1;
+
+    NFA_move(F, NFA_START_STATE, oldStates, &oldStates_len, alreadyOn);
+    
+    //for(int i=0;i<oldStates_len;i++) printf("%d ",oldStates[i]);
+    //printf("\n");
+    
+    for(int i=0;i<strlen(s);i++){
+        int tmp=getListLoc(char_set, s[i]);
+        if(tmp==-1){printf("wrong alphabet set\n"); return 0;}
+        while(oldStates_len>0){//pop stack operation
+            int t=F[oldStates[oldStates_len-1]][tmp];
+            if(t!=-1&&alreadyOn[t]==0) NFA_move(F, t, newStates, &newStates_len, alreadyOn);
+            oldStates_len--;
+        }
+        while(newStates_len>0){
+            oldStates[oldStates_len++]=newStates[newStates_len-1];
+            alreadyOn[newStates[newStates_len-1]]=0;
+            newStates_len--;
+        }
+        //for(int i=0;i<oldStates_len;i++) printf("%d ",oldStates[i]);
+        //printf("\n");            
+    }
+    for(int i=0;i<oldStates_len;i++){
+        if(oldStates[i]==NFA_STATES-1) return 1;
+    }
+    return 0;
 }
 
 
 
 int main(int argc, char **argv){
-    int control=0;
-    char s[256];
+    int control=0, **F;
+    char s[MAX_LEN];
     
     for(int i=1; i<argc; i++){
         if(strcmp(argv[i],"-para")==0) control=1;
     }
     scanf("%s",s);
     printf("%s", to_postfix(s));
-    structDFA(to_postfix(s));
+    F=structDFA(to_postfix(s));
+    while(1){
+        scanf("%s",s);
+        printf("%d\n",NFA_match(F,s));    
+    }
 }
 
 
